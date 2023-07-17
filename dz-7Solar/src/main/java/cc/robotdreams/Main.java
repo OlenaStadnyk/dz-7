d0069c997d852f1da2059005c7f204854389db59
Olena <alyonastadnick@gmail.com>
2023-07-17 13:16:49 +0300

    Initial commit

--- dz-7Solar/src/main/java/cc/robotdreams/Main.java
+++ dz-7Solar/src/main/java/cc/robotdreams/Main.java
@@ -1,2 +1,32 @@
-package cc.robotdreams;public class Main {
+package cc.robotdreams;
+
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem[] planets = SolarSystem.values(); // all planets array
+
+        for (int i = 0; i < planets.length; i++) {
+            SolarSystem planet = planets[i];
+            int ordinalNumber = i + 1; // Calculate the ordinal number
+
+            System.out.println("Ordinal number: " + ordinalNumber);
+            System.out.println("Distance from Sun: " + planet.getDistanceFromTheSun() + " for planet: " + planet.name());
+            System.out.println("Radius: " + planet.getRadius());
+
+            if (i > 0) {
+                SolarSystem previousPlanet = planets[i - 1];
+                System.out.println("Previous planet: " + previousPlanet.name());
+            } else {
+                System.out.println("Previous planet: None"); // If there is no previous planet (first planet in the enum)
+            }
+
+            SolarSystem nextPlanet = planet.getNextPlanet();
+            if (nextPlanet != null) {
+                System.out.println("Next planet: " + nextPlanet.name());
+            } else {
+                System.out.println("Next planet: None"); // If there is no next planet (last planet in the enum)
+            }
+
+            System.out.println(); // Add an empty line for better readability
+        }
+    }
 }
--- dz-7Solar/src/main/java/cc/robotdreams/SolarSystem.java
+++ dz-7Solar/src/main/java/cc/robotdreams/SolarSystem.java
@@ -1,56 +1,62 @@
 package cc.robotdreams;
 
-public enum SolarSystem2 {
-    MERCURY(0, 2439),
-    VENUS(51, 6051),
-    EARTH(41, 6371),
-    MARS(79, 3389),
-    JUPITER(550, 69911),
-    SATURN(652, 58232),
-    URANUS(1441, 25362),
-    NEPTUNE(1629, 24622);
+public enum SolarSystem {
+    MERCURY(0, 2439, null),
+    VENUS(51, 6051, MERCURY),
+    EARTH(41, 6371, VENUS),
+    MARS(79, 3389, EARTH),
+    JUPITER(550, 69911, MARS),
+    SATURN(652, 58232, JUPITER),
+    URANUS(1441, 25362, SATURN),
+    NEPTUNE(1629, 24622, URANUS);
 
-    public static final int FROM_THE_SUN_TO_MERCURY = 57;
-    private final int distanceFromCurrentToPrev;
-    private final int radius;
-    private int distanceFromTheSun;
-    private SolarSystem2 nextPlanet;
-
-    SolarSystem2(int distanceFromCurrentToPrev, int radius) {
-        this.distanceFromCurrentToPrev = distanceFromCurrentToPrev;
-        this.radius = radius;
-    }
-
-    static {
-        SolarSystem2[] planets = SolarSystem2.values();
-        for (int i = 0; i < planets.length; i++) {
-            if (i == 0) {
-                planets[i].distanceFromTheSun = FROM_THE_SUN_TO_MERCURY;
-            } else {
-                planets[i].distanceFromTheSun = planets[i - 1].distanceFromTheSun + planets[i].distanceFromCurrentToPrev;
-            }
-
-            if (i + 1 < planets.length) {
-                planets[i].nextPlanet = planets[i + 1];
-            } else {
-                planets[i].nextPlanet = null;
-            }
-        }
-    }
-
-    public int getDistanceFromCurrentToPrev() {
-        return distanceFromCurrentToPrev;
-    }
-
-    public int getRadius() {
-        return radius;
-    }
-
-    public int getDistanceFromTheSun() {
-        return distanceFromTheSun;
-    }
-
-    public SolarSystem2 getNextPlanet() {
-        return nextPlanet;
-    }
-}
+//    public static final int FROM_THE_SUN_TO_MERCURY = 57;
+//    private final int prevDistance;
+//    private final int radius;
+//    private int distanceFromTheSun;
+//    private SolarSystem nextPlanet;
+//    private SolarSystem previousPlanet; // Added previousPlanet field
+//
+//    SolarSystem(int prevDistance, int radius, SolarSystem previous) {
+//        this.prevDistance = prevDistance;
+//        this.radius = radius;
+//        this.previousPlanet = previous;
+//    }
+//
+//    static {
+//        SolarSystem[] planets = SolarSystem.values();
+//        for (int i = 0; i < planets.length; i++) {
+//            if (i == 0) {
+//                planets[i].distanceFromTheSun = FROM_THE_SUN_TO_MERCURY;
+//            } else {
+//                planets[i].distanceFromTheSun = planets[i - 1].distanceFromTheSun + planets[i].prevDistance;
+//            }
+//
+//            if (i + 1 < planets.length) {
+//                planets[i].nextPlanet = planets[i + 1];
+//            } else {
+//                planets[i].nextPlanet = null;
+//            }
+//        }
+//    }
+//
+//    public int getPrevDistance() {
+//        return prevDistance;
+//    }
+//
+//    public int getRadius() {
+//        return radius;
+//    }
+//
+//    public int getDistanceFromTheSun() {
+//        return distanceFromTheSun;
+//    }
+//
+//    public SolarSystem getNextPlanet() {
+//        return nextPlanet;
+//    }
+//
+//    public SolarSystem getPreviousPlanet() { // Getter method for previousPlanet
+//        return previousPlanet;
+//    }
+//}
